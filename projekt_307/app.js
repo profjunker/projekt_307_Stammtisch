65515a0a9f21762066f3b630e7a7d02da717f500
profjunker <tristan.buerchler@bbz-cfp.ch>
2024-11-25 09:04:37 +0100

    Changed things

--- projekt_307/app.js
+++ projekt_307/app.js
@@ -35,9 +35,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use{(req
-  req.db_connection = db_connection;
-)};
+//app.use{(req
+//  req.db_connection = db_connection;
+//)};
 
 app.use(express.static(path.join(__dirname, 'public')));
 
